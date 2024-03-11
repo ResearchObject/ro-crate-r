a9591726233a90fa0c9594785d8ba7fb1cafe1df
Marjan Meurisse <marjan.meurisse@sciensano.be>
2024-03-11 17:48:17 +0100

    Package setup

---
+++ R/add.R
@@ -0,0 +1,21 @@
+#' Title add_file
+#'
+#' Description add_file
+#'
+#' @param 
+#' @return 
+#' @export
+add_file <- function(){
+  print("Test-1")
+}
+
+#' Title add
+#'
+#' Description add
+#'
+#' @param 
+#' @return 
+#' @export
+add <- function(){
+  print("Test-2")
+}
