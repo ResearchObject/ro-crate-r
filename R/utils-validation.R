4ec20ed2e81342e8fb1e410c5954472ae4ffcac6
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-03-03 16:36:05 +0000

    Add new internal function to create a RO-Crate validation object

---
+++ R/utils-validation.R
@@ -0,0 +1,25 @@
+#' Create a \link[rocrateR]{rocrate} validation result
+#'
+#' Internal constructor for validation results.
+#'
+#' @param errors Character vector of errors.
+#' @param warnings Character vector of warnings.
+#' @param info Character vector of informational messages.
+#'
+#' @return A `rocrate_validation` object.
+#' @keywords internal
+new_rocrate_validation <- function(
+  errors = character(),
+  warnings = character(),
+  info = character()
+) {
+  structure(
+    list(
+      errors = errors,
+      warnings = warnings,
+      info = info,
+      valid = length(errors) == 0
+    ),
+    class = "rocrate_validation"
+  )
+}
